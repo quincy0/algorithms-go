dc47bab759874aadc932e7962d41dbef2c2a8426
quincy0 <wgqg51@gmail.com>
2022-08-08 10:31:22 +0800

    d220808

--- daily/daily.go
+++ daily/daily.go
@@ -477,3 +477,24 @@ func exclusiveTime(n int, logs []string) []int {
 	}
 	return ans
 }
+
+/**
+https://leetcode.cn/problems/special-binary-string/
+*/
+func makeLargestSpecial(s string) string {
+	if len(s) <= 2 {
+		return s
+	}
+	subs := sort.StringSlice{}
+	cnt, left := 0, 0
+	for i, ch := range s {
+		if ch == '1' {
+			cnt++
+		} else if cnt--; cnt == 0 {
+			subs = append(subs, "1"+makeLargestSpecial(s[left+1:i])+"0")
+			left = i + 1
+		}
+	}
+	sort.Sort(sort.Reverse(subs))
+	return strings.Join(subs, "")
+}
