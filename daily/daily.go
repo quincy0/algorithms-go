e29717fc4502c6931be70d614a5c66542bc997ab
quincy0 <wgqg51@gmail.com>
2022-08-25 11:11:28 +0800

    d220825

--- daily/daily.go
+++ daily/daily.go
@@ -1115,3 +1115,23 @@ func canBeEqual(target []int, arr []int) bool {
 	}
 	return true
 }
+
+/*
+https://leetcode.cn/problems/find-k-closest-elements/
+时间复杂度：O(logN + k)
+空间复杂度：O(1)
+*/
+func findClosestElements(arr []int, k int, x int) []int {
+	right := sort.SearchInts(arr, x)
+	left := right - 1
+	for ; k > 0; k-- {
+		if left < 0 {
+			right++
+		} else if right >= len(arr) || x-arr[left] <= arr[right]-x {
+			left--
+		} else {
+			right++
+		}
+	}
+	return arr[left+1 : right]
+}
