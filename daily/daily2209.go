648115d46c63f1395b54e92a97d5160cdd1694f3
quincy0 <wgqg51@gmail.com>
2022-09-10 22:47:43 +0800

    d220910

--- daily/daily2209.go
+++ daily/daily2209.go
@@ -229,3 +229,49 @@ func minOperations(logs []string) int {
 	}
 	return deep
 }
+
+/**
+https://leetcode.cn/problems/trim-a-binary-search-tree
+*/
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	}
+	if root.Val > high {
+		return trimBST(root.Left, low, high)
+	}
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
+}
+
+func trimBSTII(root *TreeNode, low, high int) *TreeNode {
+	for root != nil && (root.Val < low || root.Val > high) {
+		if root.Val < low {
+			root = root.Right
+		} else {
+			root = root.Left
+		}
+	}
+	if root == nil {
+		return nil
+	}
+	for node := root; node.Left != nil; {
+		if node.Left.Val < low {
+			node.Left = node.Left.Right
+		} else {
+			node = node.Left
+		}
+	}
+	for node := root; node.Right != nil; {
+		if node.Right.Val > high {
+			node.Right = node.Right.Left
+		} else {
+			node = node.Right
+		}
+	}
+	return root
+}
