7016f2cb043ebf31aef0f2c9914e003b93b009b6
quincy0 <wgqg51@gmail.com>
2022-09-19 10:35:46 +0800

    d220919

--- daily/daily2209.go
+++ daily/daily2209.go
@@ -558,3 +558,18 @@ func largestIsland(grid [][]int) (ans int) {
 	}
 	return
 }
+
+/**
+https://leetcode.cn/problems/sort-array-by-increasing-frequency/
+*/
+func frequencySort(nums []int) []int {
+	cnt := map[int]int{}
+	for _, x := range nums {
+		cnt[x]++
+	}
+	sort.Slice(nums, func(i, j int) bool {
+		a, b := nums[i], nums[j]
+		return cnt[a] < cnt[b] || cnt[a] == cnt[b] && a > b
+	})
+	return nums
+}
